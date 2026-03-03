4f3d92f6d3868a10403c91552514e03350cfb50c
robogogasensei <robotogasensei@gmail.com>
2026-03-03 23:38:28 +0900

    refactor

--- sim/main.py
+++ sim/main.py
@@ -2,7 +2,6 @@ import pybullet as p
 import pybullet_data
 import time
 import math
-import mcl
 import numpy as np
 
 from util import get_joint_index, get_link_index
@@ -57,18 +56,6 @@ odom = Odometry(
     initial_theta=init_yaw
 )
 
-mcl = mcl.MCL(
-    num_particles=30,
-    robot_id=robot_id,
-    lidar_link=lidar_link,
-    wheel_radius=wheel_radius,
-    wheel_base=wheel_base,
-    dt=dt,
-    init_x=init_pos[0],
-    init_y=init_pos[1],
-    init_theta=init_yaw
-)
-
 def angle_diff(a, b):
     """
     角度差を -π〜π に正規化
@@ -114,26 +101,18 @@ while True:
     odom.step()
     odom_x, odom_y, odom_theta = odom.get_state()
 
-    # ===== MCL更新 =====
-    omega_L, omega_R = odom.read_wheel_encoders()
-    mcl.motion_update(omega_L, omega_R)
-    mcl.sensor_update(distances)
-    mcl.resample()
-    mcl_x, mcl_y, mcl_theta = mcl.estimate()
-
     # ===== 真値取得 =====
     true_pos, true_orn = p.getBasePositionAndOrientation(robot_id)
     _, _, true_theta = p.getEulerFromQuaternion(true_orn)
 
     # ===== 表示 =====
     theta_err_odom = angle_diff(true_theta, odom_theta)
-    theta_err_mcl  = angle_diff(true_theta, mcl_theta)
 
     print(
-        f"X: ({true_pos[0]:.2f}, {odom_x:.2f}, {mcl_x:.2f})  "
-        f"Y: ({true_pos[1]:.2f}, {odom_y:.2f}, {mcl_y:.2f})  "
-        f"θ: ({true_theta:.2f}, {odom_theta:.2f}, {mcl_theta:.2f})  "
-        f"θerr(O:{theta_err_odom:.3f}, M:{theta_err_mcl:.3f})"
+        f"X: (Tr {true_pos[0]:.2f}, Od {odom_x:.2f})  "
+        f"Y: (Tr {true_pos[1]:.2f}, Od {odom_y:.2f})  "
+        f"θ: (Tr {true_theta:.2f}, Od {odom_theta:.2f})  "
+        f"θerr(O:{theta_err_odom:.3f})"
     )
     p.stepSimulation()
     time.sleep(dt)
